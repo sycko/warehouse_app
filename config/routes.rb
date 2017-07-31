9b22507f37b3c5eb64609444e4288abf54834a99
Liam Skyes <liam.sykes15@gmail.com>
2017-07-31 11:06:35 -0400

    finished static pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,6 @@
 Rails.application.routes.draw do
+  root 'static_pages#home'
   get 'static_pages/home'
-
   get 'static_pages/warehouses'
-
   get 'static_pages/products'
-
-  root 'application#hello'
 end
--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,19 +1,27 @@
 require 'test_helper'
 
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
+
+  def setup
+    @base_title = "Warehouse App"
+  end
+  
   test "should get home" do
     get static_pages_home_url
     assert_response :success
+    assert_select "title", "Home | #{@base_title}"
   end
 
   test "should get warehouses" do
     get static_pages_warehouses_url
     assert_response :success
+    assert_select "title", "Warehouses | #{@base_title}"
   end
 
   test "should get products" do
     get static_pages_products_url
     assert_response :success
+    assert_select "title", "Products | #{@base_title}"
   end
 
 end
